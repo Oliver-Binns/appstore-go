9537242849125e8b06185a9076a02a0dc5e828af
Oliver Binns <mail@oliverbinns.co.uk>
2025-06-28 13:08:04 +0100

    feat: add ability to delete users (#10)

---
+++ users/delete.go
@@ -0,0 +1,36 @@
+package users
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+
+	"github.com/oliver-binns/googleplay-go/networking"
+)
+
+func Delete(c networking.HTTPClient, ctx context.Context, rawURL string, id string) error {
+	// Parse the raw URL
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return fmt.Errorf("failed to parse URL: %w", err)
+	}
+	parsedURL.Path = path.Join(parsedURL.Path, "users", id)
+
+	// Create the HTTP request
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, parsedURL.String(), http.NoBody)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	resp, err := c.Do(req)
+
+	if resp.StatusCode == http.StatusNoContent {
+		return nil
+	} else if err != nil {
+		return fmt.Errorf("failed to delete user: %w", err)
+	} else {
+		return fmt.Errorf("unexpected response code: %d", resp.StatusCode)
+	}
+}
